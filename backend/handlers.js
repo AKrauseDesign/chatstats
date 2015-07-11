2528a3b71b304cf86e98d5899d7154c3ad9dadef
TimohStudios <timogos.k@live.nl>
2015-07-12 01:32:22 +0200

    Some more cleanup. Little bit opiniated, undo if you prefer

--- backend/handlers.js
+++ backend/handlers.js
@@ -1,34 +1,34 @@
 'use strict';
 var app = require('express')(),
-http = require('http').Server(app),
-io = require('socket.io')(http);
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
 // Temp Vars
 var KPM = 0;
 
-module.exports = {
-  chat: function(channel, user, message, self){
-    console.log(user);
-    io.emit('stats', {
-      user: {
-        user: user['display-name'],
-        color: user.color,
-        sub : user.subscriber,
-        turbo: user.turbo
-      },
-      msg: message,
-      kpm: KPM,
-      date: Date.now()
-    });
-  },
+module.chat = function(channel, user, message, self){
+  console.log(user);
+  io.emit('stats', {
+    user: {
+      user: user['display-name'],
+      color: user.color,
+      sub : user.subscriber,
+      turbo: user.turbo
+    },
+    msg: message,
+    kpm: KPM,
+    date: Date.now()
+  });
+};
 
-  timeout: function(channel, username){
+module.timeout = function(channel, username){
 
-  },
-  subscriber: function(channel, username){
+};
 
-  },
-  subanniversary: function(channel, username, months){
+module.subscriber = function(channel, username){
 
-  }
-}
+};
+
+module.subanniversary = function(channel, username, months){
+
+};
