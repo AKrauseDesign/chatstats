0524fcad27a638e0833a42b9e6b7b6ae7c18ee45
TimohStudios <timogos.k@live.nl>
2015-07-13 14:52:22 +0200

    Started on the subcombo code

--- backend/handlers/subscriber.js
+++ backend/handlers/subscriber.js
@@ -1,8 +1,24 @@
+var currentCombo = 2;
+var comboEvent = 'subscriber-combo';
+
 module.exports = function(client, io) {
-  client.addListener('subscriber', function (channel, user, message) {
-    io.emit('search', {
-      test: 'dank memes'
+  var countdown = setTimeout(function() {
+    currentCombo = 0;
+    io.emit(comboEvent, {
+      combo: -1,
+      lastSubscriber: null
     });
-    console.log('emit done');
+  }, 60 * 1000);
+  
+  client.addListener('subscriber', function (channel, user, message) {
+
+
+    currentCombo++;
+    if (currentCombo > 1) {
+      io.emit(comboEvent, {
+        combo: currentCombo,
+        lastSubscriber: 'Timo'
+      });
+    }
   });
 };
