e8e40682b01a27fa8025bf80b8d4817170ad9101
Styler <stylergaminghd@gmail.com>
2015-07-19 15:02:43 +0200

    WatchedTime: FIX

--- backend/handlers/watchedTime.js
+++ backend/handlers/watchedTime.js
@@ -1,12 +1,6 @@
 module.exports = function(client, db) {
   var users = {};
 
-  client.addListener('chat', function(channel, user, message) {
-    if(message === '!test' && username === 'stylerdev' || username === 'timohstudios') {
-      console.log(users);
-    }
-  });
-
   client.addListener('join', function (channel, username) {
     console.log('JOINED: '+username);
     users[username] = Date.now();
@@ -17,10 +11,16 @@ module.exports = function(client, db) {
     var joined = users[username];
     var watched = now - joined;
     delete users[username];
-    console.log('LEFT: ' + username + '  WATCHED: ' + watched);
-    if(users[username] === null || users[username] === undefined) {console.log('It works!');} else {console.log('NOPE!');}
-    // TODO: Push watched to DB
-    // SUGGESTION: Move this code to an event, make the handler the DB code
+    if(users[username] === null || users[username] === undefined) {
+      var time = Math.round(watched / 1000 / 60);
+      db.Users.findOrCreate(
+        {where:
+          { name: username },
+      }).spread(function(user) {
+        user.increment('watchedTime', {by: time});
+        console.log('Watchedtime: '+username+' By: '+time+' min');
+      });
+    } else {
+      console.log('Bailing out, you are on your own. Good luck!');}
   });
-
 };
