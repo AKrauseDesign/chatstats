9753adf5d814aab0b5e9948cdaf7e117cc0bb855
Styler <stylergaminghd@gmail.com>
2015-07-22 17:31:20 +0200

    Search Upgrade + fixes for subs

--- frontend/app/scripts/controllers/search.js
+++ frontend/app/scripts/controllers/search.js
@@ -21,10 +21,19 @@ angular.module('websiteApp')
         if(angular.isUndefined(data) || data === null ) {
           $scope.foundUser = 3;
         } else {
+          $http.jsonp("https://api.twitch.tv/kraken/users/"+data.name+"?callback=JSON_CALLBACK").
+          success(function(data) {
+            if(data.logo !== null) {
+              $scope.avatar = data.logo;
+            } else {
+              $scope.avatar = 'http://stylerdev.io/images/no_avatar.jpg';
+            }
+          });
           $scope.name = data.name;
           $scope.watchedTime = data.watchedTime;
           $scope.count = data.count;
           $scope.lastMsg = data.lastMsg;
+          $scope.lastMsgDate = data.updatedAt;
           $scope.foundUser = 2;
         }
       }).error(function(data){
