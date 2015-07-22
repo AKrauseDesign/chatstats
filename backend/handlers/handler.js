79f4b254bce2c135abc4d5f6e5425070026926e1
TimohStudios <timogos.k@live.nl>
2015-07-22 22:10:21 +0200

    UNTESTED: More emote attempts

--- backend/handlers/handler.js
+++ backend/handlers/handler.js
@@ -4,76 +4,11 @@ var emoteHandler    = require('./emotes');
 var subEmoteHandler = require('./subEmotes');
 var commandHandler  = require('./commands');
 var request         = require('request');
-var kpmodule        = require('../handlers/kpm');
+var kpmodule        = require('./kpm');
+var data            = require('./startup');
 
-var globalEmotes = [];
-var subEmotes = [
-  {
-    id: 6115,
-    code: 'masDoge'
-  },
-  {
-    id: 10367,
-    code: 'masBM'
-  },
-  {
-    id: 12760,
-    code: 'masGasm'
-  },
-  {
-    id: 35600,
-    code: 'masKappa'
-  },
-  {
-    id: 35601,
-    code: 'masW'
-  },
-  {
-    id: 54064,
-    code: 'masThump'
-  },
-  {
-    id: 54065,
-    code: 'masHey'
-  },
-  {
-    id: 54395,
-    code: 'masGame'
-  },
-  {
-    id: 54371,
-    code: 'masFail'
-  },
-  {
-    id: 54370,
-    code: 'masC'
-  }
-];
-// request('https://api.twitch.tv/kraken/chat/massansc/emoticons', function(err, res, body) {
-//   console.warn('Fired request');
-//   if(!err && res.statusCode === 200) {
-//     emotes = JSON.parse(body);
-//     emotes = emotes.emoticons;
-//     for (var emote in emotes) {
-//       if (emotes[emote].subscriber_only === true) {
-//         subEmotes.push(emotes[emote]);
-//       } else {
-//         globalEmotes.push(emotes[emote]);
-//       }
-//     }
-//   } else {
-//     console.error('Couldn\'t get subscriber emotes.');
-//   }
-// });
-
-request('http://api.twitch.tv/kraken/chat/emoticon_images?emotesets=0', function(err, res, body) {
-// request('http://api.twitch.tv/kraken/chat/emoticon_images', function(err, res, body) {
-  if (!err && res.statusCode === 200) {
-    globalEmotes = JSON.parse(body);
-    globalEmotes = globalEmotes.emoticon_sets[0];
-    allEmotes = subEmotes.concat(globalEmotes);
-  }
-});
+var globalEmotes = data.globalEmotes;
+var subEmotes = data.subEmotes;
 
 module.exports = function(userObj, message) {
   var user = userObj['display-name'];
@@ -96,34 +31,6 @@ module.exports = function(userObj, message) {
     }
   }
 
-
-
-
-  // WIP EMOTES
-  // var formatEmotes = function(emotes) {
-  //   for(var emote in emotes) {
-  //     for(var i = 0; i < subEmotes.length; i++) {
-  //       if (emote === subEmotes[i].regex) {
-  //         // TODO: Hook up to Massan's channel to see if this works
-  //         // This will insert it into the submote DB but also in the normal emote DB, we need to prevent that from happening still in some way
-  //         subEmoteHandler(emote, emotes[emote].length, user, message);
-  //       }
-  //     }
-  //     var subStrArray = emotes[emote];
-  //     if(emoteObject.hasOwnProperty(emote)) {
-  //       emoteObject[emote] =+ subStrArray.length;
-  //     } else {
-  //       emoteObject[emote] = subStrArray.length;
-  //     }
-  //   }
-  // };
-  //
-  // var callEmoteDB = function(emoteObject) {
-  //   for (var emote in emoteObject) {
-  //     emoteHandler(emote, emoteObject[emote], user, message);
-  //   }
-  // };
-  // callEmoteDB(emoteObject);
 
   // Emotes
 
@@ -159,7 +66,7 @@ module.exports = function(userObj, message) {
   var emoteObject = {};
 
   for (var current = 0; current < globalEmotes.length; current++) {
-    var name = globalEmotes[current].code;
+    var name = globalEmotes[current].regex;
     for (var i = 0; i < words.length; i++) {
       if (words[i] === name) {
         if(emoteObject.hasOwnProperty(name)) {
---
+++ backend/handlers/startup.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+var globalEmotes = [];
+var subEmotes = [];
+
+request('https://api.twitch.tv/kraken/chat/massansc/emoticons', function(err, res, body) {
+  console.warn('Fired request');
+  if(!err && res.statusCode === 200) {
+    emotes = JSON.parse(body);
+    emotes = emotes.emoticons;
+    for (var emote in emotes) {
+      if (emotes[emote].subscriber_only === true) {
+        subEmotes.push(emotes[emote]);
+      } else {
+        globalEmotes.push(emotes[emote]);
+      }
+    }
+  } else {
+    console.error('Couldn\'t get emotes.');
+  }
+});
+
+module.exports = {
+  globalEmotes: globalEmotes,
+  subEmotes: subEmotes
+}
