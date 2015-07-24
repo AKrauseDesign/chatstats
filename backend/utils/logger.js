8ecec4e20a3dd03cef66d8dbaac1905b2067393d
TimohStudios <timogos.k@live.nl>
2015-07-25 01:33:56 +0200

    Fixed logs

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,16 +1,22 @@
-var logger = require('winston');
+var winston = require('winston');
+var path = require('path');
 
+var infofile = path.join(__dirname, '../logs/info.log');
+var errfile = path.join(__dirname, '../logs/error.log');
 
-
-logger.add(logger.transports.File, {
-  name: 'general-log',
-  filename: 'logs/info.log'
-});
-
-logger.add(logger.transports.File, {
-  name: 'error-log',
-  filename: 'logs/error.log',
-  level: 'error'
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({
+      name: 'general-log',
+      filename: infofile
+    }),
+    new (winston.transports.File)({
+      name: 'error-log',
+      filename: errfile,
+      level: 'error'
+    }),
+  ]
 });
 
 module.exports = logger;
