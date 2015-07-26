44c113d8855c5daac07aca1f2a7b87fe78898106
TimohStudios <timogos.k@live.nl>
2015-07-26 12:35:58 +0200

    Make emotes dependant on config, attempt fix for null emote

--- backend/handlers/startup.js
+++ backend/handlers/startup.js
@@ -1,18 +1,21 @@
 var request = require('request');
 var logger = require('../utils/logger');
+var config = require('../config');
 var globalEmotes = [];
 var subEmotes = [];
 
-request('https://api.twitch.tv/kraken/chat/massansc/emoticons', function(err, res, body) {
+request('https://api.twitch.tv/kraken/chat/' + config.identity.channels[0] + '/emoticons', function(err, res, body) {
   logger.info('Fired emote request');
   if(!err && res.statusCode === 200) {
     emotes = JSON.parse(body);
     emotes = emotes.emoticons;
     for (var emote in emotes) {
-      if (emotes[emote].subscriber_only === true) {
-        subEmotes.push(emotes[emote]);
-      } else {
-        globalEmotes.push(emotes[emote]);
+      if(emotes[emote] !== null) {
+        if (emotes[emote].subscriber_only === true) {
+          subEmotes.push(emotes[emote]);
+        } else {
+          globalEmotes.push(emotes[emote]);
+        }
       }
     }
   } else {
