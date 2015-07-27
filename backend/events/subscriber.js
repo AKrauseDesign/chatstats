5dc338530cd47751af2e9ccad3b173651dbfdd48
Styler <stylergaminghd@gmail.com>
2015-07-28 01:20:18 +0200

    Fixed all issues with subscribers not displaying / sending 1000 messages. #BibleThump

--- backend/events/subscriber.js
+++ backend/events/subscriber.js
@@ -1,36 +1,19 @@
-var currentCombo = 0;
-var comboEvent = 'subscriber-combo';
 var minuteToHour = require('../utils/minutetohour');
 
 module.exports = function(client, io, db) {
-  // var countdown = setTimeout(function() {
-  //   currentCombo = 0;
-  //   io.emit(comboEvent, {
-  //     combo: -1,
-  //     lastSubscriber: null
-  //   });
-  // }, 60 * 1000);
 
-  client.addListener('subscriber', function (channel, username) {
+  client.on("subscription", function (channel, username) {
     client.subscribers(channel).then(function() {
       setTimeout(function () {
         client.subscribersoff(channel);
       }, 10*1000);
     });
     db.Users.findOne({ where: {name: username} }).then(function(sub) {
-      if(!sub) {
-        client.say('MaSsanSC', "xanHY xanPE Welcome to the hood" + username +" You've written 0 Lines of text so far xanLove");
+      if(typeof sub !== 'undefined') {
+        client.say(channel, 'xanHY xanPE Welcome to the hood ' + username + " You've written " + sub.count + " lines of text so far and been in chat for " + minuteToHour(sub.watchedTime) + " hours xanLove");
       } else {
-        client.say('MaSsanSC', 'xanHY xanPE Welcome to the hood ' + username + " You've written " + sub.count + " lines of text so far and been in chat for " + minuteToHour(sub.watchedTime) + " hours xanLove");
+        client.say(channel, "xanHY xanPE Welcome to the hood" + username +" You've written 0 Lines of text so far xanLove");
       }
     });
-
-    // currentCombo++;
-    // if (currentCombo > 1) {
-    //   io.emit(comboEvent, {
-    //     combo: currentCombo,
-    //     lastSubscriber: username
-    //   });
-    // }
   });
 };
